2eaa97279ede52c9031d87d6880df80ea5b74c2e
banjoboss73 <ayush.jpenkar@gmail.com>
2022-12-04 17:06:07 +0000

    commiting all initial

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+// Javascript for Header
+  const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+  const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+  const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+  const header = document.querySelector('.header.container');
+
+  hamburger.addEventListener('click', () => {
+	  hamburger.classList.toggle('active');
+	  mobile_menu.classList.toggle('active');
+  });
+
+  document.addEventListener('scroll', () => {
+	  var scroll_position = window.scrollY;
+	  if (scroll_position > 250) {
+	  	header.style.backgroundColor = '#29323c';
+	  } else {
+	  header.style.backgroundColor = 'transparent';
+  	}
+  });
+
+  menu_item.forEach((item) => {
+	  item.addEventListener('click', () => {
+		  hamburger.classList.toggle('active');
+		  mobile_menu.classList.toggle('active');
+	  });
+  });
+
+//End of Javascript
+
+// Javascript for Slides
+let slideIndex = [1, 1];
+let slideId = ["mySlides1", "mySlides2"];
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+}
+
