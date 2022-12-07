8131be11a82c7626062043caaf56071d8c7f285c
banjoboss73 <ayush.jpenkar@gmail.com>
2022-12-07 22:22:11 +0000

    eslint installed

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
-// Javascript for Header
-  const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-  const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-  const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-  const header = document.querySelector('.header.container');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
 
-  hamburger.addEventListener('click', () => {
-	  hamburger.classList.toggle('active');
-	  mobile_menu.classList.toggle('active');
+
+  mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = '#29323c';
+  } else {
+	header.style.backgroundColor = 'transparent';
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
   });
+});
 
-  document.addEventListener('scroll', () => {
-	  var scroll_position = window.scrollY;
-	  if (scroll_position > 250) {
-	  	header.style.backgroundColor = '#29323c';
-	  } else {
-	  header.style.backgroundColor = 'transparent';
-  	}
-  });
-
-  menu_item.forEach((item) => {
-	  item.addEventListener('click', () => {
-		  hamburger.classList.toggle('active');
-		  mobile_menu.classList.toggle('active');
-	  });
-  });
-
-//End of Javascript
+// End of Javascript
 
 // Javascript for Slides
 let slideIndex = [1, 1];
-let slideId = ["mySlides1", "mySlides2"];
+let slideId = ['mySlides1', 'mySlides2'];
 showSlides(1, 0);
 showSlides(1, 1);
 
@@ -49,9 +49,9 @@ function showSlides(n, no) {
     slideIndex[no] = x.length;
   }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = 'none';
   }
-  x[slideIndex[no] - 1].style.display = "block";
+  x[slideIndex[no] - 1].style.display = 'block';
 }
 
 // Data to make pie charts
