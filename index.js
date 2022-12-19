739f3815dcc8743e95aaa01765393fbf5a766cc9
banjoboss73 <ayush.jpenkar@gmail.com>
2022-12-20 02:55:11 +0530

    data added

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ menuItem.forEach((item) => {
   });
 });
 
-// End of Javascript
-
 // Javascript for Slides
 const slideIndex = [1, 1];
 const slideId = ['mySlides1', 'mySlides2'];
@@ -53,93 +51,52 @@ function showSlides(n, no) {
   }
   x[slideIndex[no] - 1].style.display = 'block';
 }
-import {PieChart} from "@d3/pie-chart"
 
-// Data to make pie charts
-function PieChart (data, {
-  name = ([x]) => x, // given d in data, returns the (ordinal) label
-  value = ([, y]) => y, // given d in data, returns the (quantitative) value
-  title, // given d in data, returns the title text
-  width = 640, // outer width, in pixels
-  height = 400, // outer height, in pixels
-  innerRadius = 0, // inner radius of pie, in pixels (non-zero for donut)
-  outerRadius = Math.min(width, height) / 2, // outer radius of pie, in pixels
-  labelRadius = (innerRadius * 0.2 + outerRadius * 0.8), // center radius of labels
-  format = ',', // a format specifier for values (in the label)
-  names, // array of names (the domain of the color scale)
-  colors, // array of colors for names
-  stroke = innerRadius > 0 ? 'none' : 'white', // stroke separating widths
-  strokeWidth = 1, // width of stroke separating wedges
-  strokeLinejoin = "round", // line join of stroke separating wedges
-  padAngle = stroke === "none" ? 1 / outerRadius : 0, // angular separation between wedges
-} = {}) {
-  // Compute values.
-  const N = d3.map(data, name);
-  const V = d3.map(data, value);
-  const I = d3.range(N.length).filter(i => !isNaN(V[i]));
+// Javascript for doughnut chart
 
-  // Unique the names.
-  if (names === undefined) names = N;
-  names = new d3.InternSet(names);
+// set the dimensions and margins of the graph
+const width = 450;
+const height = 450;
+const margin = 40;
 
-  // Chose a default color scheme based on cardinality.
-  if (colors === undefined) colors = d3.schemeSpectral[names.size];
-  if (colors === undefined)colors=d3.quantize(t=>d3.interpolateSpectral(t * 0.8 + 0.1), names.size);
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width, height) / 2 - margin
 
-  // Construct scales.
-  const color = d3.scaleOrdinal(names, colors);
+// append the svg object to the div called 'my_dataviz'
+const svg = d3.select('#my_dataviz')
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`);
 
-  // Compute titles.
-  if (title === undefined) {
-    const formatValue = d3.format(format);
-    title = i => `${N[i]}\n${formatValue(V[i])}`;
-  } else {
-    const O = d3.map(data, d => d);
-    const T = title;
-    title = i => T(O[i], i, data);
-  }
+// Create dummy data
+const data = { Argentine: 139, Australian: 56, Austrian: 243, Belgian: 89, Brazillian: 155,
+  British: 835, Canadian:196, Chilean: 61, Chinese: 81, Colombian: 42, Croatian: 25,
+  Cuban: 58, Czech:83, Danish: 119, Dutch: 265, Finnish:60, French: 839, German: 930, Hungarian:52, 
+  Indian: 28, Irish:19, Israeli: 75, Italian:531, Japanese: 598, Korean: 35, Mexican:128, Norwegian:49, 
+  Peruvian:34, Polish: 125, Russian: 188, Scottish: 28, South African: 69, Spanish: 153, Swedish: 130, Swiss: 280,
+  Turkish: 26, Venezuelan:41,Yugoslav: 41 }
 
-  // Construct arcs.
-  const arcs = d3.pie().padAngle(padAngle).sort(null).value(i => V[i])(I);
-  const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
-  const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
-  
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-width / 2, -height / 2, width, height])
-      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+// set the color scale
+const color = d3.scaleOrdinal()
+  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
 
-  svg.append("g")
-      .attr("stroke", stroke)
-      .attr("stroke-width", strokeWidth)
-      .attr("stroke-linejoin", strokeLinejoin)
-    .selectAll("path")
-    .data(arcs)
-    .join("path")
-      .attr("fill", d => color(N[d.data]))
-      .attr("d", arc)
-    .append("title")
-      .text(d => title(d.data));
+// Compute the position of each group on the pie:
+const pie = d3.pie()
+  .value(d=>d[1])
 
-  svg.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("text-anchor", "middle")
-    .selectAll("text")
-    .data(arcs)
-    .join("text")
-      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-    .selectAll("tspan")
-    .data(d => {
-      const lines = `${title(d.data)}`.split(/\n/);
-      return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
-    })
-    .join("tspan")
-      .attr("x", 0)
-      .attr("y", (_, i) => `${i * 1.1}em`)
-      .attr("font-weight", (_, i) => i ? null : "bold")
-      .text(d => d);
+const data_ready = pie(Object.entries(data))
 
-  return Object.assign(svg.node(), {scales: {color}});
-}
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('whatever')
+  .data(data_ready)
+  .join('path')
+  .attr('d', d3.arc()
+    .innerRadius(100)         // This is the size of the donut hole
+    .outerRadius(radius))
+  .attr('fill', d => color(d.data[0]))
+  .attr('stroke', 'black')
+  .style('stroke-width', '2px')
+  .style('opacity', 0.7);
